129d8ae825b95987cc5995a5578418df331fa070
Joseph D. Purcell <josephdpurcell@gmail.com>
2015-09-04 08:39:27 -0500

    Rename gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var server = require('gulp-server-livereload');
 
-gulp.task('webserver', function() {
+gulp.task('serve', function() {
   gulp.src('./')
     .pipe(server({
       livereload: true,
