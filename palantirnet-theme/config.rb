0d452044b4af97e68ed9de06e93ab78b4daf6c97
Joseph D. Purcell <josephdpurcell@gmail.com>
2015-09-15 20:58:36 -0500

    Update palantir theme

--- palantirnet-theme/config.rb
+++ palantirnet-theme/config.rb
@@ -2,10 +2,10 @@ require 'breakpoint'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "assets/css"
-sass_dir = "assets/sass"
-images_dir = "assets/img"
-javascripts_dir = "assets/js"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
